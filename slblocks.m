9440f9c0735fdb5f1fc7f9c1288ee8858527b94f
tkw954 <t.wiens@usask.ca>
2016-06-10 11:33:40 -0600

    Fix slblocks.m

--- slblocks.m
+++ slblocks.m
@@ -1,6 +1,6 @@
 function blkStruct = slblocks
   % Specify that the product should appear in the library browser
   % and be cached in its repository
-  Browser.Library = 'usask_tlm';
+  Browser.Library = 'usask_tlm_lib';
   Browser.Name    = 'USask Transmission Line Model Library';
   blkStruct.Browser = Browser;
