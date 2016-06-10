d11db3fa3bd21dbb019db6547587ead97cd7d1de
tkw954 <t.wiens@usask.ca>
2016-06-10 11:24:34 -0600

    Initial Commit

---
+++ slblocks.m
@@ -0,0 +1,6 @@
+function blkStruct = slblocks
+  % Specify that the product should appear in the library browser
+  % and be cached in its repository
+  Browser.Library = 'usask_tlm';
+  Browser.Name    = 'USask Transmission Line Model Library';
+  blkStruct.Browser = Browser;
